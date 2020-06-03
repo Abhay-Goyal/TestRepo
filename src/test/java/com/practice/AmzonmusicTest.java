5e7c643ddddbede390a66c843efc27e58a14e692
abhay <1994abhigoyal@gmail.com>
2020-06-03 11:18:42 +0530

    Updated Log4j File

--- src/test/java/com/practice/AmzonmusicTest.java
+++ src/test/java/com/practice/AmzonmusicTest.java
@@ -54,7 +54,7 @@ public class AmzonmusicTest extends DriverInitialization {
 	@DataProvider
 	private Object[][] getData() {
 		Object[][] data = new Object[1][2];
-		data[0][0] = "abc@yopmail.com";
+		data[0][0] = "8699446654";
 		data[0][1] = "test123";
 		return data;
 
